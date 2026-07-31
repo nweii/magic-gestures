ddc7d85e1c279b78b83fe2eb564beab3513e3380
Nathan Cheng <nthn.wei@gmail.com>
2026-07-31 13:51:07 -0400

    Start version numbering at 0.1.0
    The bundle carried 0.1.1 from the fork it was built on, describing that
    author's releases rather than this project's.

--- scripts/build.sh
+++ scripts/build.sh
@@ -32,9 +32,9 @@ cat > "$APP_BUNDLE/Contents/Info.plist" <<PLIST
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>0.1.1</string>
+  <string>0.1.0</string>
   <key>CFBundleVersion</key>
-  <string>2</string>
+  <string>1</string>
   <key>LSMinimumSystemVersion</key>
   <string>13.0</string>
   <key>LSUIElement</key>
