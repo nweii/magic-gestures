05b3cb1a780bf4e064825577959783e49f69d834
Nathan Cheng <nthn.wei@gmail.com>
2026-08-01 09:28:11 -0400

    Release 0.3.0

--- scripts/build.sh
+++ scripts/build.sh
@@ -32,9 +32,9 @@ cat > "$APP_BUNDLE/Contents/Info.plist" <<PLIST
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>0.2.2</string>
+  <string>0.3.0</string>
   <key>CFBundleVersion</key>
-  <string>4</string>
+  <string>5</string>
   <key>LSMinimumSystemVersion</key>
   <string>13.0</string>
   <key>LSUIElement</key>
